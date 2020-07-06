eea6e174f771646f5b1f3d50a7d7f2d81932a3a2
Tzippy Freedman <tzippyfreedman1@gmail.com>
2020-07-06 21:49:48 +0300

    register and login ui

--- client/Weight-Monitor/src/app/login/login.component.ts
+++ client/Weight-Monitor/src/app/login/login.component.ts
@@ -25,7 +25,9 @@ export class LoginComponent implements OnInit {
     this.loginForm = new FormGroup({
       'username': new FormControl('',[
       Validators.required,
-      Validators.minLength(2)
+      Validators.minLength(2),
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")
+
 
     ]),
     'password': new FormControl('',[
--- client/Weight-Monitor/src/app/register/register.component.ts
+++ client/Weight-Monitor/src/app/register/register.component.ts
@@ -39,7 +39,9 @@ export class RegisterComponent implements OnInit {
       
       
         'email': new FormControl('',[
-          Validators.required]),
+          Validators.required,
+          Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")
+        ]),
           'password': new FormControl('',[
             Validators.required,
              Validators.minLength(6)]),
